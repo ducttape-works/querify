199968e9b88a8e701ffa87a15cc97cd663d41e8a
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2026-05-14 19:32:47 +0100

    chore: add posthog to track web analytics

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
 
-createRoot(document.getElementById('root')!).render(
+import "./index.css";
+import App from "./App.tsx";
+
+posthog.init(import.meta.env.VITE_POSTHOG_KEY, {
+  api_host: "https://us.i.posthog.com",
+  defaults: "2026-01-30",
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
